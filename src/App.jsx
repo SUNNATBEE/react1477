db0b42cd100a8b3fb7aa7c97bb80b51e4ba779a1
SUNNATBEE <yusupovsunnatbek32@gmail.com>
2025-07-03 15:42:51 +0500

    Button qoshdim

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div className='bg-red-500'>
+Salom komanddoshlar
 
+<button className='btn btn-primary'>clikc</button>
     </div>
   )
 }
